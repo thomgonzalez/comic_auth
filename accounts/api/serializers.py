5ca37a10ec7c39e8f3531b7315b08b085fc81bd5
Tomás Santiago <tsantiago@nomi.mx>
2022-05-16 01:38:02 -0500

    Format code.

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -3,31 +3,30 @@ from rest_framework import serializers
 from accounts.actions import PerfileAction
 
 
-
 class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-    age = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = User
-        fields = (
-            'id', 
-            'name', 
-            'age',
-        )
-        
-    def get_name(self, obj):
-        first_name = obj.first_name
-        last_name = obj.last_name
-        
-        return f'{first_name} {last_name}'
-    
-    def get_age(self, obj):
-        perfil = PerfileAction()
-        queryset = perfil.get(**{'user': obj})
-        return queryset.age if queryset else None
+	name = serializers.SerializerMethodField()
+	age = serializers.SerializerMethodField()
+
+	class Meta:
+		model = User
+		fields = (
+			'id',
+			'name',
+			'age',
+		)
+
+	def get_name(self, obj):
+		first_name = obj.first_name
+		last_name = obj.last_name
+
+		return f'{first_name} {last_name}'
+
+	def get_age(self, obj):
+		perfil = PerfileAction()
+		queryset = perfil.get(**{'user': obj})
+		return queryset.age if queryset else None
 
 
 class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(max_length=128)
+	username = serializers.CharField(max_length=150)
+	password = serializers.CharField(max_length=128)
--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from accounts.api.views import (
-    TestView, 
-    UserViewSet, 
-    LoginAuthToken
+	TestView,
+	UserViewSet,
+	LoginAuthToken
 )
 
-
 urlpatterns = [
-    path("test/", TestView.as_view()),
-    path('register/', UserViewSet.as_view({'post': 'create'}), name='user'),
-    path('login/', LoginAuthToken.as_view(), name='login'),
+	path("test/", TestView.as_view()),
+	path('register/', UserViewSet.as_view({'post': 'create'}), name='user'),
+	path('login/', LoginAuthToken.as_view(), name='login'),
 ]
