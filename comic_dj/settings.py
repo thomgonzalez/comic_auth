64469128a8476c88ee37d5122742e39eda6ca946
Tomás Santiago <tsantiago@nomi.mx>
2022-05-15 19:42:36 -0500

    Postgresql database connection.

--- comic_dj/settings.py
+++ comic_dj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'comic_dj.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": {
+		"ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+		"NAME": os.environ.get("POSTGRES_DB", "postgres"),
+		"USER": os.environ.get("POSTGRES_USER", "user"),
+		"PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+		"HOST": os.environ.get("POSTGRES_SERVER", "localhost"),
+		"PORT": os.environ.get("POSTGRES_PORT", "5432"),
+	}
 }
+
 
 
 # Password validation
